7191b58fa6ed8d4569ccae9f2cb147c8d6bcee54
Nikita Etinger <113473133+Nikita-Etinger@users.noreply.github.com>
2023-06-30 21:12:00 +0300

    Add files via upload

---
+++ Patternodinichka.cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Pressedent {
+private:
+    string fName;
+   static Pressedent* activPresedent;
+    Pressedent(string fName) {
+        this->fName = fName;
+    }
+public:
+    static Pressedent*  getPressedent(string newNamefName) {
+        if (activPresedent == nullptr) {
+            activPresedent =new Pressedent(newNamefName);
+        }
+        return activPresedent;
+    }
+    void whoeInfo() {
+        cout<< this << fName<<endl;
+    }
+};
+Pressedent* Pressedent::activPresedent = nullptr;
+int main()
+{
+    Pressedent* pres= Pressedent::getPressedent("putin");
+    Pressedent* prestwo = Pressedent::getPressedent("aaaaaaaa");
+    pres->whoeInfo();
+    prestwo->whoeInfo();
+    std::cout << "Hello World!\n";
+}
+
